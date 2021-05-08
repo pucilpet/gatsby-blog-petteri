a85d3bed510bac96827b6ad1a52e33680f36a7ec
Petteri Pucilowski <petteri@hersecret.fi>
2021-05-08 22:16:25 +0300

    added business category, removed redundant categories

---
+++ content/blog/business/hello-world---petteris-first-blog.md
@@ -0,0 +1,8 @@
+---
+title: Hello World - Petteris first blog
+date: 2021-05-08 22:05:56
+category: business
+thumbnail: { thumbnailSrc }
+draft: false
+---
+
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: `Petteri Pucilowski´s personal blog`,
-  description: `Blog posted about ...`,
+  description: `I will document my daily learnings on doing business online and the technical side of the it.`,
   author: `[Petteri Pucilowski]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  introduction: `My learnings on doing business online and the technical side of the it. The main focus is developing the customer journey.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: `https://twitter.com/pucilpet`, // Your Twitter account
