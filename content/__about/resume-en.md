2c2d837d5dd1b9366b246778d4718ad38c76e210
Petteri Pucilowski <petteri@hersecret.fi>
2021-05-08 22:08:19 +0300

    updated meta config

--- content/__about/resume-en.md
+++ content/__about/resume-en.md
@@ -8,6 +8,10 @@ lang: 'en'
 
 <div align="center">
 
-_Hello! My name is Petteri Pucilowski - welcome to my blog! I run an online retail business with my wife - the main focus is women's clothing and our main market is Finland. In this blog I will document my daily learnings regarding the online business and maybe I'll touch on my personal life, too. If you have any questions, please email_
+Hello! My name is Petteri Pucilowski - welcome to my blog! I run an online retail business with my wife - the main focus is women's clothing and our main market is Finland. In this blog I will document my daily learnings regarding the online business and maybe I'll touch on my personal life, too. If you have any questions, please email me at <a href="mailto:petteri@pucilowski.eu">petteri@pucilowski.eu</a>
+
+Right now I am focusing on improving the customer service side of our business and creating automatic processes to increase customer satisfaction. I am doing this with WhatsApp and Chat bots. Also we are looking to expand our services into neighboring countries (Sweden, Estonia) in the near future.
+
+In my many years of doing business online I have learned how to build everything from the ground up - be it server setup and maintenance or setting up a Prestashop storefront - that's the advantage of doing everything yourself and using open-source software. I am fluent in building scalable solutions using Docker and Kubernetes. I also enjoy learning new technologies and at the moment I am learning React programming.
 
 </div>
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `Petteri Pucilowski´s personal blog`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
+  author: `[Petteri Pucilowski]`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `https://twitter.com/pucilpet`, // Your Twitter account
+    github: `https://github.com/pucilpet`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
-    instagram: ``, // Your Instagram account
+    facebook: `https://www.facebook.com/petteri.pucilowski`, // Your Facebook account
+    linkedin: `https://www.linkedin.com/in/petteri-pucilowski-a0921a99/`, // Your LinkedIn account
+    instagram: `https://www.instagram.com/hersecretfi/`, // Your Instagram account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/profile.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'pucilpet/gatsby-blog-petteri', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'pucilpet',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
